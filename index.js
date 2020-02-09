5d8b5d72c96c68d5f6892a8dc11acefdc60bd5b5
Zi Ye <18612319597@163.com>
2020-02-09 22:20:06 +0800

    [feature] Signaling service

---
+++ index.js
@@ -0,0 +1,44 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8010});
+const code2ws = new Map();
+
+wss.on('connection', (ws, request) => {
+    const code = Math.floor(Math.random() * (999999 - 100000)) + 10000;
+    code2ws.set(code, ws);
+    ws.sendData = (event, data) => {
+      ws.send(JSON.stringify({ event, data }));
+    };
+    ws.on('message', (message) => {
+        console.log('incoming', message);
+        let parseMessage = {};
+        try {
+            parseMessage = JSON.parse(message);
+        } catch(e) {
+            console.log('parse message error', e);
+            return;
+        }
+        const { event, data } = parseMessage;
+        if(event === 'login') {
+            ws.sendData('logined', { code });
+        }
+        if(event === 'control') {
+            const remote = +data.remote;
+            if(code2ws.has(remote)) {
+                ws.sendData('controlled', { remote });
+                ws.sendRemote = code2ws.get(remote).sendData;
+                ws.sendRemote('be-controlled', { remote: code });
+            }
+        }
+        if(event === 'forward') {
+            ws.sendRemote(data.event, data.data);
+        }
+    });
+    ws.on('close', () => {
+        code2ws.delete(code);
+        clearTimeout(ws._closeTimeout);
+    });
+    ws._closeTimeout = setTimeout(() => {
+        ws.terminate();
+    }, 10 * 60 * 1000);
+});
